d4392002873719819ba1bbe66946ee09b033d570
Shannon Denna <sdenna@d211.org>
2021-09-12 15:38:54 -0500

    Added icons to my options menu and adjusted the colors

--- app/src/main/java/com/example/optionsmenuexampleproject/MainActivity.java
+++ app/src/main/java/com/example/optionsmenuexampleproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.optionsmenuexampleproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -18,10 +20,27 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    /* Looked up why the icons weren't dispalying with teh options menu.  Apparently
+       it is now required to make the icons visible.  So I added the if statement shown
+       below based on a suggestion shown in this SO post.  I also needed to add
+       @SuppressLint before the method to be allowed to use this method.  I'm not quite
+       sure why all this was needed, but without it, I wasn't able to get the icons to show up
+
+        https://stackoverflow.com/questions/19750635/icon-in-menu-not-showing-in-android
+
+
+     */
     @Override
+    @SuppressLint("RestrictedApi")
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
+
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+
         return true;
     }
 
